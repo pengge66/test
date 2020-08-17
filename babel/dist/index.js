a35cb32bc80f21ab0835efdd09e060a8c9decff5
wangguopeng <wangguopeng@021.com>
2020-08-17 16:32:08 +0800

    firstcommit

---
+++ babel/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var wgp = 1;
+var a = Number(wgp) + 2;
+console.log(wgp);
+console.log(a);
---
+++ babel/src/index.js
@@ -0,0 +1,4 @@
+const wgp = 1;
+let a = Number(wgp) + 2;
+console.log(wgp);
+console.log(a);
